d7635ed99b413c7345ba8444508990142f5a2169
harshita-kona <harshitanlrncs@gmail.com>
2021-03-31 20:38:45 +0530

    Create dependency to validate API key

--- app/src/dependencies.py
+++ app/src/dependencies.py
@@ -1,6 +1,7 @@
 from fastapi import Header, HTTPException
+from app.src.config import get_config
 
 async def check_apikey(api_key:str=Header(None, convert_underscores=False)):
-    key=""
+    key=get_config()['api_key']
     if api_key!=key:
         raise HTTPException(status_code=400, detail="X-Token header invalid")
