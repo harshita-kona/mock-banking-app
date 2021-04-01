d26f51147a04acfc9cc1c8c8f59fd782f5f20571
harshita-kona <harshitanlrncs@gmail.com>
2021-04-01 18:50:38 +0530

    Add dependency for db connection

--- app/src/dependencies.py
+++ app/src/dependencies.py
@@ -1,7 +1,16 @@
 from fastapi import Header, HTTPException
 from app.src.config import get_config
+from app.connect_database import SessionLocal,engine
 
 async def check_apikey(api_key:str=Header(None, convert_underscores=False)):
     key=get_config()['api_key']
     if api_key!=key:
         raise HTTPException(status_code=400, detail="X-Token header invalid")
+
+async def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
