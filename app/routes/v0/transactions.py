87ecbf2b195ea4a8d04bbc7849f3802729ecd401
harshita-kona <harshitanlrncs@gmail.com>
2021-04-06 19:00:47 +0530

    Add config

--- app/routes/v0/transactions.py
+++ app/routes/v0/transactions.py
@@ -2,7 +2,7 @@ import json
 from fastapi import APIRouter, Request, Depends, FastAPI
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
-from app.src.dependencies import check_apikey, get_db, get_current_user
+from app.src.dependencies import check_apikey, get_db, get_current_user, get_current_active_user
 from app.src.utils import get_error_code
 from app import database, schemas
 from sqlalchemy.orm import Session
@@ -13,7 +13,7 @@ router = APIRouter()
 API to add transaction of the user
 """
 @router.post('/addtransaction/')
-def account_signup(req:schemas.TransactionCreate ,valid_user=Depends(get_current_user),db: Session = Depends(get_db), dependencies=Depends(check_apikey)):
+def add_transaction(req:schemas.TransactionCreate, active_user=Depends(get_current_active_user),valid_user=Depends(get_current_user),db: Session = Depends(get_db), dependencies=Depends(check_apikey)):
     message=database.add_user_transaction(db=db,transaction= req)
     if not message:
         message=get_error_code("insufficient_balance")
@@ -32,7 +32,7 @@ def account_signup(req:schemas.TransactionCreate ,valid_user=Depends(get_current
 API to get last n transactions of the account
 """
 @router.post('/lastntransactions/')
-def account_signup(req:schemas.LastnTransactions ,valid_user=Depends(get_current_user),db: Session = Depends(get_db), dependencies=Depends(check_apikey)):
+def get_last_n_transactions(req:schemas.LastnTransactions, active_user=Depends(get_current_active_user), valid_user=Depends(get_current_user),db: Session = Depends(get_db), dependencies=Depends(check_apikey)):
     message=database.get_n_transactions(db=db,transaction= req)
     message=jsonable_encoder(message)
     return JSONResponse(message)
@@ -42,8 +42,9 @@ def account_signup(req:schemas.LastnTransactions ,valid_user=Depends(get_current
 API to get all the transactions done between the two dates
 """
 @router.post('/transactionsbydate/')
-def account_signup(req:schemas.TransactionsBetweenDates ,valid_user=Depends(get_current_user),db: Session = Depends(get_db), dependencies=Depends(check_apikey)):
+def get_transactions_by_date(req:schemas.TransactionsBetweenDates, active_user=Depends(get_current_active_user), valid_user=Depends(get_current_user),db: Session = Depends(get_db), dependencies=Depends(check_apikey)):
     message=database.get_transactions_between_dates(db=db,transaction= req)
     message=jsonable_encoder(message)
 
     return JSONResponse(message)
+    
